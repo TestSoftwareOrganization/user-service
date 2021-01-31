eb97fe46e63f4a791ad3667f00f2e1f862d49522
kolevtest <kolevtestsoftware@gmail.com>
2021-02-01 00:01:18 +0200

    Authentication is configured (very simple way)

--- src/main/java/com/revo/eshop/userservice/repositories/UsersRepository.java
+++ src/main/java/com/revo/eshop/userservice/repositories/UsersRepository.java
@@ -4,5 +4,5 @@ import com.revo.eshop.userservice.data.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsersRepository extends CrudRepository<UserEntity, Long> {
-
+    UserEntity findByEmail(String email);
 }
--- src/main/java/com/revo/eshop/userservice/service/UsersService.java
+++ src/main/java/com/revo/eshop/userservice/service/UsersService.java
@@ -1,7 +1,10 @@
 package com.revo.eshop.userservice.service;
 
 import com.revo.eshop.userservice.shared.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService {
     UserDto createUser(UserDto userDetails);
+
+    UserDto getUserDetailsByEmail(String Email);
 }
